683318e9ab63657b3f5f5b88a1c983925d38c4d4
Chanon Jenakom <chanonjenakom@gmail.com>
2017-05-12 16:33:14 +0700

    Create login activity screen

--- app/src/main/java/com/example/mickeycj/todos/login/LoginActivity.java
+++ app/src/main/java/com/example/mickeycj/todos/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.mickeycj.todos.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.mickeycj.todos.R;
 
@@ -11,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onSignUpTabClick(View view) {
+    }
+
+    public void onLoginClick(View view) {
     }
 }
