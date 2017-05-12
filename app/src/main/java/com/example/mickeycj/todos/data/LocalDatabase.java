04cf636c22481938e8f19598237f7e096955f51c
Chanon Jenakom <chanonjenakom@gmail.com>
2017-05-12 12:58:07 +0700

    Create local database

---
+++ app/src/main/java/com/example/mickeycj/todos/data/LocalDatabase.java
@@ -0,0 +1,38 @@
+package com.example.mickeycj.todos.data;
+
+/**
+ * Created by mickeycj on 12/5/2560.
+ */
+
+public class LocalDatabase extends AbstractDatabase {
+
+    public static Database getInstance() {
+        if (instance == null) {
+            instance = new LocalDatabase();
+        }
+        return instance;
+    }
+
+    private LocalDatabase() {
+        super();
+        User user1 = new User("mickey@gmail.com", "mickey", "12345678");
+        Todo todo11 = new Todo("Prepare for exams");
+        Todo todo12 = new Todo("Finish Arrowverse");
+        user1.addTodo(todo11);
+        user1.addTodo(todo12);
+        user1.addItemTo(0, new Item("Read Software Specification & Design"));
+        user1.addItemTo(1, new Item("Finish The Flash"));
+        user1.addItemTo(1, new Item("Finish The Arrow"));
+        user1.addItemTo(1, new Item("Finish Supergirl"));
+        users.put(user1.getEmail(), user1);
+        User user2 = new User("barry@starlabs.com", "barry", "12345678");
+        Todo todo21 = new Todo("Save Iris");
+        Todo todo22 = new Todo("Eat Big Belly Burgurs");
+        user2.addTodo(todo21);
+        user2.addTodo(todo22);
+        user2.addItemTo(0, new Item("Steal power cell for Speedforce Bazooka"));
+        user2.addItemTo(0, new Item("Trap Savitar in the Speedforce"));
+        user2.addItemTo(1, new Item("Go out with Team Flash"));
+        users.put(user2.getEmail(), user2);
+    }
+}
