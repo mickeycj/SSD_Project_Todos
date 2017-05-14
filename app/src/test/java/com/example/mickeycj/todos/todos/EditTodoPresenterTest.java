205aae10cc5ec07936342a265b3776622094a541
Chanon Jenakom <chanonjenakom@gmail.com>
2017-05-14 13:32:44 +0700

    Finish unit test for edit-todo activity

---
+++ app/src/test/java/com/example/mickeycj/todos/todos/EditTodoPresenterTest.java
@@ -0,0 +1,48 @@
+package com.example.mickeycj.todos.todos;
+
+import com.example.mickeycj.todos.data.Todo;
+import com.example.mickeycj.todos.data.User;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.inOrder;
+
+/**
+ * Created by mickeycj on 14/5/2560.
+ */
+
+public class EditTodoPresenterTest {
+
+    private EditTodoPresenter presenter;
+
+    @Mock
+    private EditTodoView view;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        User user = new User("test@gmail.com", "test", "password");
+        user.addTodo(new Todo("Test todo", false));
+        presenter = new EditTodoPresenter(user, 0, view);
+    }
+
+    @Test
+    public void shouldClearEditTextAndCheckboxAfterStart() {
+        presenter.start();
+        InOrder order = inOrder(view);
+        order.verify(view).setTodoNameEditText();
+        order.verify(view).setImportantCheckBox();
+    }
+
+    @Test
+    public void shouldGetValuesFromEditTextAndCheckboxAfterSubmit() {
+        presenter.submit();
+        InOrder order = inOrder(view);
+        order.verify(view).getTodoNameFromEditText();
+        order.verify(view).isImportantCheckboxChecked();
+    }
+}
